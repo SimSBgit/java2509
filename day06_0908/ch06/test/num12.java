161a3bf8b20f6a82cefb51720a169247d3847026
SimSBgit <sbct47@gmail.com>
2025-09-10 08:45:06 +0900

    수정
    0910 / 1교시

---
+++ day06_0908/ch06/test/num12.java
@@ -0,0 +1,10 @@
+package ch06.test;
+
+public class num12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day07_0909/ch07/exercise/num6.java
@@ -0,0 +1,10 @@
+package ch07.exercise;
+
+public class num6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
