e04931b2b4646061d868ce478211c5fdf1f3a836
SimSBgit <sbct47@gmail.com>
2025-09-16 17:29:57 +0900

    수정
    09 16 막교시

---
+++ day12_0916/ch13/sec02/exam01/Product.java
@@ -0,0 +1,12 @@
+package ch13.sec02.exam01;
+
+public class Product <K, M>{
+
+	private K kind;
+	private M model;
+	
+	public K getKind() { return this.kind; }
+	public M getModel() { return this.model; }
+	public K setKind(K kind) { return this.kind = kind; }
+	public M setModel(M model) { return this.model = model; }
+}
